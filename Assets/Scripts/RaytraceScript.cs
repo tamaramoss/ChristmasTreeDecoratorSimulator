6bd30872973cb292d48c14c7971618776d14c490
LAPTOP-1DTL49NQ\tamar <tamara.moss1997@gmail.com>
2021-04-24 15:35:47 +0200

    Thow and aim

---
+++ Assets/Scripts/RaytraceScript.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaytraceScript : MonoBehaviour
+{
+    RaycastHit hitInfo;
+    public Image Aim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward, Color.magenta);
+        Physics.Raycast(this.transform.position, this.transform.forward, out hitInfo);
+        ColorAim();
+    }
+
+    void ColorAim()
+    {
+        if (hitInfo.transform && hitInfo.transform.gameObject.CompareTag("decoration"))
+        {
+            Debug.Log(hitInfo.transform.gameObject.tag);
+
+            Aim.color = Color.green;
+        }
+        else
+        {
+            Aim.color = Color.red;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/ThrowScript.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using Cinemachine;
+
+public class ThrowScript : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerCam;
+    public float throwForce = 10;
+    public float pickUpDistance = 2.5f;
+    bool hasPlayer = false;
+    bool beingCarried = false;
+    // public AudioClip[] soundToPlay;
+    // private AudioSource audio;
+    public int dmg;
+    private bool touched = false;
+
+    void Start()
+    {
+        //audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // float dist = Vector3.Distance(gameObject.transform.position, player.position);
+        // if (dist <= pickUpDistance)
+        // {
+        //     hasPlayer = true;
+        // }
+        // else
+        // {
+        //     hasPlayer = false;
+        // }
+        if (Input.GetMouseButtonDown(1) && !beingCarried)
+        {
+            Debug.Log("Pick up");
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = playerCam;
+            playerCam.GetComponent<CinemachineVirtualCamera>().LookAt = transform;
+            beingCarried = true;
+        }
+        if (beingCarried)
+        {
+            // if (touched)
+            // {
+            //     GetComponent<Rigidbody>().isKinematic = false;
+            //     transform.parent = null;
+            //     beingCarried = false;
+            //     touched = false;
+            // }
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
+                RandomAudio();
+            }
+            // else if (Input.GetMouseButtonDown(1))
+            // {
+            //     GetComponent<Rigidbody>().isKinematic = false;
+            //     transform.parent = null;
+            //     beingCarried = false;
+            // }
+        }
+    }
+    void RandomAudio()
+    {
+        // if (audio.isPlaying){
+        //     return;
+        //         }
+        // audio.clip = soundToPlay[Random.Range(0, soundToPlay.Length)];
+        // audio.Play();
+
+    }
+
+    }
